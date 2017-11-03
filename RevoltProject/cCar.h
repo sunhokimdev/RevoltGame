a210e6857f5979191c0b02a0a9bf7d8653324c49
이태섭 <tsyi313@naver.com>
2017-11-03 18:26:12 +0900

    //자동차 속도 기록

--- RevoltProject/cCar.h
+++ RevoltProject/cCar.h
@@ -28,7 +28,7 @@ class cCar : public Object
 	float m_maxWheelAngle;	//바퀴가 꺽이는 최대값. 
 	float m_wheelAcc;		//꺽이는 속도	(비율에 더해지는 값)
 
-	float m_fCurrentSpeed;
+	SYNTHESIZE(float, m_fCurrentSpeed, CurrentSpeed);
 	D3DXVECTOR3 m_szPrevPos[5];
 
 	//AI
